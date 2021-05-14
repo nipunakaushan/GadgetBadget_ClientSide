30cf4d4a823260801a11b7051beb342cc1e9253f
it19117010 <it19117010@my.sliit.lk>
2021-05-14 21:03:59 +0530

    Data base configuration

--- PAF-Assignemnt2/src/com/ApprovementApi.java
+++ PAF-Assignemnt2/src/com/ApprovementApi.java
@@ -16,7 +16,7 @@ import model.Approvement;;
 /**
  * Servlet implementation class DoctorAPI
  */
-@WebServlet("/AppointmentAPI")
+@WebServlet("/ApprovementAPI")
 public class ApprovementApi extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
